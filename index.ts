1a5e6ea1056e30a089dc5de0607def110c5b0b29
Chris533 <ganchengs@gmail.com>
2020-07-31 12:29:20 +0800

    Update to alpha.3 (rename, add bunadmin)

---
+++ index.ts
@@ -0,0 +1,4 @@
+export * from "./types"
+
+export { default as dataCtrl } from "./controllers/dataCtrl";
+export { default as graphqlSer } from "./services/graphqlSer"
--- services/graphqlSer.ts
+++ services/graphqlSer.ts
@@ -1,8 +1,5 @@
-import request from "@/utils/scripts/request"
-import storedToken from "@/utils/scripts/storedToken"
-import { ENV } from "@/utils/config"
+import { request, storedToken, ENV, notice } from "@bunred/bunadmin";
 import { ServiceProps } from "../types"
-import { notice } from "@/core"
 import i18next from "i18next"
 
 export default async function graphqlSer({
